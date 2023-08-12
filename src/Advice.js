713fe9c96d4e445e16a43f376693b2d5ebcb98f9
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2023-08-13 02:45:26 +0800

    add error handling and transitions

--- src/Advice.js
+++ src/Advice.js
@@ -1,28 +1,52 @@
 import React, { useState, useEffect } from "react"
+import Error from "./Error"
 import Divider from "./images/pattern-divider-mobile.svg"
 import DividerDesktop from "./images/pattern-divider-desktop.svg"
 import Dice from "./images/icon-dice.svg"
 
 const Advice = () => {
+    const [error, setError] =  useState(null)
     const [advice, setAdvice] = useState({
         "id": "",
         "advice": ""
       })
-    
     const [count, setCount] = useState(0)
-
-    const getAdvice = () => setCount(prevCount => prevCount + 1)
+    const [isVisible, setIsVisible] = useState(false)
+    const getAdvice = () => {
+      setCount(prev => prev + 1)
+      setIsVisible(false)
+    }
 
     useEffect(() => {
-      fetch("https://api.adviceslip.com/advice")
-        .then(response => response.json())
-        .then(adviceData => setAdvice(adviceData.slip))
-      }, [count])
+      let fetchWorking = true
+      if (fetchWorking) {
+          fetch("https://api.adviceslip.com/advice")
+          .then(res => {
+            if (res.ok) {
+              return res.json()
+            } else {
+              throw new Error("error")
+            }})
+          .then(data => {
+            setAdvice(data.slip)
+            setIsVisible(true)
+          })
+          .catch(e => {
+            console.error(e)
+            setError(true)
+          })
+      }
+      return () => fetchWorking = false
+    }, [count])
     
+    if (error) return <Error />
     return (
         <div className="advice--container">
-            <h1 className="advice--id">Advice #{advice.id}</h1>
-            <p className="advice--text">"{advice.advice}"</p>
+            <h1 className="advice--id">
+              Advice
+              <span className={isVisible ? "visible" : "non-visible"}> #{advice.id}</span>
+            </h1>
+            <p className={`advice--text ${isVisible ? "visible" : "non-visible"}`}>"{advice.advice}"</p>
             <picture>
               <source media="(min-width: 600px)" srcSet={DividerDesktop}/>
               <img src={Divider} alt="pattern divider" className="advice--divider"/>
---
+++ src/Error.js
@@ -0,0 +1,9 @@
+const Error = () => {
+    return(
+        <div className="error--page">
+            <img className="error--gif" src="https://media.giphy.com/media/xT9IgIc0lryrxvqVGM/giphy.gif" alt="error gif" />
+            <h1 className="error--message">Oops! Something went wrong. Please try again later.</h1>
+        </div>
+    )
+}
+export default Error
